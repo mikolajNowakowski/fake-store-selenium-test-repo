f42e0af997db414b72cdd5ba4b96dd31c0621f33
Mikołaj Nowakowski <mikolajnowakowski99@gmail.com>
2024-06-10 16:28:46 +0200

    initial commit

---
+++ src/main/java/com/app/FakeStoreTestFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeStoreTestFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakeStoreTestFrameworkApplication.class, args);
+	}
+
+}
